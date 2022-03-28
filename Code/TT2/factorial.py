f27503a97d0a3201d96ce6cd3c10da6439d41266
Krishnadev Lakshminarayanan <89166946+KrishnadevL@users.noreply.github.com>
2022-03-28 21:35:03 +0000

    made some last min changes to factorial

--- Code/TT2/factorial.py
+++ Code/TT2/factorial.py
@@ -1,7 +1,4 @@
 class Factorial:
-    def __init__(self):
-      # defines the self, so it can be called again
-        self.factor = [0, 1]
     def __call__(self, n):
         num = 1
         # conducts factorial formula multiplying previous value 
@@ -10,9 +7,12 @@ class Factorial:
 
         return num #recursive 
       
+      
 def main():
-  user_num = int(input("Choose a number:"))
+  user_ans = input("Choose a number:")
+  user_num = int(user_ans)
   fact_num = Factorial() # object instantiation and run __init__ method
+  print("The factorial for the number "+user_ans+" is:")
   print(fact_num(user_num)) # object running __call__ method
 
 if __name__ == "__main__":
